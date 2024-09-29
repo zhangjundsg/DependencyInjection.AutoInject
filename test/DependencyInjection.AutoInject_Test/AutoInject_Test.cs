81ac97980e5c8a0c1393f0c057be90eaee6a59da
zhangjun <2859487085@qq.com>
2024-09-29 21:54:51 +0800

    补交

---
+++ test/DependencyInjection.AutoInject_Test/AutoInject_Test.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DependencyInjection.AutoInject_Test
+{
+    public class AutoInject_Test
+    {
+        [Fact]
+        public void Test1()
+        {
+            var services = new ServiceCollection();
+            
+        }
+    }
+}
